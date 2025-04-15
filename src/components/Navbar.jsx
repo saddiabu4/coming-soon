d72e3df161f83e7e300d3ab80ace9900c11cecf0
Abdulatif <saddiabu4@gmail.com>
2025-04-15 10:15:03 +0500

    coming soon sahifa birinchi push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -178,33 +178,7 @@ const Navbar = () => {
 					{/* Mobile menu button */}
 					<div className="md:hidden flex items-center">
 						<LanguageSelect onChange={handleLanguageChange} value={language} />
-						<button
-							onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-							className="ml-4 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-[#ff9600] focus:outline-none"
-						>
-							<svg
-								className="h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								{isMobileMenuOpen ? (
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M6 18L18 6M6 6l12 12"
-									/>
-								) : (
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M4 6h16M4 12h16M4 18h16"
-									/>
-								)}
-							</svg>
-						</button>
+						
 					</div>
 				</div>
 			</div>
